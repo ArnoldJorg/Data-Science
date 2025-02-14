73f941011606df486142e5c2ea11b644a5d67d5a
Arnold <arnold.mobio1@outlook.com>
2025-02-14 15:30:43 +0000

    Worked on the youtube api and finished 3/4 challenges

--- session5/youtube_api.py
+++ session5/youtube_api.py
@@ -1,4 +1,5 @@
 import os
+from collections import Counter
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -11,7 +12,11 @@ load_dotenv()
 youtube_api_key = os.getenv("YOUTUBE_API_KEY")
 youtube = build("youtube", "v3", developerKey=youtube_api_key)
 request = youtube.videos().list(
-    part="snippet,statistics", chart="mostPopular", regionCode="US", maxResults=10
+    part="snippet,statistics",
+    chart="mostPopular",
+    regionCode="US",
+    maxResults=10,
+    videoCategoryId=0,
 )
 response = request.execute()
 
@@ -27,6 +32,7 @@ for item in response["items"]:
             "Views": int(item["statistics"]["viewCount"]),
             "Likes": int(item["statistics"].get("likeCount", 0)),
             "Comments": int(item["statistics"].get("commentCount", 0)),
+            "CategoryId": item["snippet"]["categoryId"],
         }
     )
 
@@ -62,18 +68,6 @@ for item in comment_response["items"]:
     sentiments.append(sentiment)
 
 comment_df = pd.DataFrame({"Comment": comments, "Sentiment": sentiments})
-comment_df.head()
-
-comments = []
-sentiments = []
-
-for item in comment_response["items"]:
-    comment = item["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
-    sentiment = TextBlob(comment).sentiment.polarity  # Analyzes sentiment
-    comments.append(comment)
-    sentiments.append(sentiment)
-
-comment_df = pd.DataFrame({"Comment": comments, "Sentiment": sentiments})
 print(comment_df.head())
 
 plt.hist(comment_df["Sentiment"], bins=20, color="green", alpha=0.7)
@@ -81,3 +75,48 @@ plt.xlabel("Sentiment Score")
 plt.ylabel("Frequency")
 plt.title("YouTube Comment Sentiment Analysis")
 plt.show()
+
+# category_request = youtube.videoCategories().list(part="snippet", regionCode="US")
+# category_response = category_request.execute()  # Use category_request here
+
+# for item in category_response["items"]:
+#     title = item["snippet"].get("title", "No title provided")
+#     print(title)
+
+video_df
+
+
+# Extra challenge 1: Find out which category of videos gets the most views (this is out of the maxResult set).
+top_5_categories = Counter(video_df["CategoryId"]).most_common()
+
+print("these are the most common 5 categories", top_5_categories)
+
+# Extra challenge 2: Compare comments vs. likes.
+
+sum_of_comments = video_df["Comments"].sum()
+sum_of_likes = video_df["Likes"].sum()
+
+print(sum_of_likes)
+print(sum_of_comments)
+
+# There are more likes than comments
+
+plt.scatter(video_df["Likes"], video_df["Comments"], alpha=0.7)
+plt.xlabel("Likes")
+plt.ylabel("Comments")
+plt.title("Comparison of Likes vs Comments")
+plt.show()
+
+# 3. Find the most commonly used words in YouTube comments.
+
+joined_comment = " ".join(comments)
+#  Now I have a long sentence
+# print(joined_comment) -- to see if it works as expected (which it did)
+# seperate it on each word again into an array
+
+array_words = joined_comment.split()
+
+common_word = Counter(array_words).most_common()
+print(common_word)
+
+# Note: I am working with live data so this is changeing all the time
